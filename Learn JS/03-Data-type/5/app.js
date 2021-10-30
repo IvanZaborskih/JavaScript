77ed54eb16328d23f012a744a8c1147093282fd0
IvanZaborskih <ivan.31.03@mail.ru>
2021-10-30 16:25:15 +0300

    update

--- Learn JS/03-Data-type/5/app.js
+++ Learn JS/03-Data-type/5/app.js
@@ -102,9 +102,133 @@ arr.slice([start], [end])
 // console.log(arr);
 
 // split и join
+// let names = 'Иван, Лиза, Гена, Вася, Лола';
+// let arr = names.split(', ');
+// console.log(arr);
+// arr = arr.join('; ');
+// console.log(arr);
+
+// reduce/reduceRight
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.reduce((sum, currentItem) => sum + currentItem, 0);
+// console.log(result);
+
+// Array.isArray  -  Он возвращает true, если value массив, и false, если нет
+// console.log(Array.isArray([]));     // true
+// console.log(Array.isArray({}));     // false
 
 
 
+//      Задачи
+// Переведите текст вида border-left-width в borderLeftWidth
+// function camelize(str) {
+//     let words = str.split('-');
+//     // words = words.map((word, index) => (index === 0) ? word : word[0].toUpperCase() + word.slice(1));
+//     words = words.map((word, index) => {
+//         if (index === 0) {
+//             return word;
+//         } else {
+//             return word = word[0].toUpperCase() + word.slice(1);
+//         }
+//     });
 
+//     return words.join('');
+// }
+// console.log(camelize("background-color"));
+// console.log(camelize("list-style-image"));
+// console.log(camelize("-webkit-transition"));
+
+
+// Фильтрация по диапазону
+// const filterRange = (arr, indexStart, indexEnd) => {
+//     let numBetween = arr.filter(item => {
+//         if (item >= indexStart || item <= indexEnd) {
+//             return item;
+//         }
+//     });
+
+//     return numBetween;
+// }
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr, 1, 4);
+// console.log(arr);
+// console.log(filtered);
+
+
+// Фильтрация по диапазону "на месте"
+// const filterRangeInPlace = (arr, indexStart, indexEnd) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < indexStart || arr[i] > indexEnd) {
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//     }
+// };
+// let arr = [5, 5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4);  // удалены числа вне диапазона 1..4
+// console.log(arr);  // [3, 1]
+
+
+// Сортировать в порядке по убыванию
+// let arr = [5, 2, 1, -10, 2, 8, 22, -10];
+// arr.sort((a, b) => b - a);
+// console.log(arr);
+
+
+// Скопировать и отсортировать массив
+// function copySorted(arr) {
+//     let sorted = arr.slice();
+//     return sorted.sort();
+// }
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let sorted = copySorted(arr);
+// console.log(sorted); // CSS, HTML, JavaScript
+// console.log(arr); // HTML, JavaScript, CSS (без изменений)
+
+
+// Создать расширяемый калькулятор
+// function Calculator() {
+//     this.metods = {
+//         '-': (a, b) => a - b,
+//         '+': (a, b) => a + b
+//     };
+
+//     this.calculate = function (str) {
+//         let arr = str.split(' ');
+//         let a = +arr[0];
+//         let op = arr[1];
+//         let b = +arr[2];
+
+//         return this.metods[op](a, b);
+//     };
+
+//     this.addMethod = function (name, func) {
+//         this.metods[name] = func;
+//     }
+// }
+// let calc = new Calculator();
+// console.log(calc.calculate('3 + 7'));
+// console.log(calc.calculate('10 - 7'));
+// console.log(calc.calculate('13 - 90'));
+
+// let powerCalc = new Calculator();
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+
+// console.log(powerCalc.calculate('13 * 5'));
+// console.log(powerCalc.calculate('20 / 4'));
+// console.log(powerCalc.calculate('2 ** 3'));
+
+
+// Трансформировать в массив имён
+let users = [
+    { name: "Вася", age: 25 },
+    { name: "Петя", age: 30 },
+    { name: "Маша", age: 28 }
+]
+
+let names = users.filter(item => item.name);
+console.log(names);
 
 
