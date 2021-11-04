996673ba6c11612caa37015c223018f079e1c0cb
IvanZaborskih <ivan.31.03@mail.ru>
2021-11-04 22:05:26 +0300

    sony

---
+++ Learn JS/DOM/01-Document/1/app.js
@@ -0,0 +1,15 @@
+// console.log(window.innerHeight);
+
+//  DOM (Document Object Model)
+// document.body.style.backgroundColor = 'red';
+// setTimeout(() => document.body.style.backgroundColor = '', 3000);
+
+//  BOM (Browser Object Model)
+// console.log(navigator.userAgent);
+// console.log(navigator.platform);
+
+// console.log(location.href)
+// if (confirm('Перейти в гугл?')) {
+//     location.href = 'http://google.com'
+// }
+
