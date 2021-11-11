c0c3589df02a812d0ff7d39f975d45f116743fbe
IvanZaborskih <ivan.31.03@mail.ru>
2021-11-11 19:29:04 +0300

    up

---
+++ Learn JS/DOM/01-Document/5/app.js
@@ -0,0 +1,19 @@
+// console.log(document.body);
+// console.dir(document.body);
+
+// console.log(document.body.innerHTML);
+// document.body.innerHTML = 'new HTML';
+
+// console.log(document.querySelector('#elem').outerHTML);
+
+// let div = document.querySelector('div');
+// div.outerHTML = '<p>Новый элемент</p>';
+// console.log(div.outerHTML);
+
+// const inner = document.querySelector('#elem');
+// console.log(inner.innerHTML);
+// console.log(inner.textContent);
+
+// let name = prompt('введите ваше имя');
+// document.querySelector('#elem1').innerHTML = name;
+// document.querySelector('#elem2').textContent = name;
--- Test/app.js
+++ Test/app.js
@@ -1,19 +1,4 @@
-// function upArray(arr) {
-//     for (let item of arr) {
-//         if (item > 9 || !isFinite(item)) {
-//             return null;
-//         }
-//     }
-//     let ar = Number(arr.join(''));
-//     // console.log(ar);
-//     if (isNaN(ar)) {
-//         return null;
-//     }
-//     ar++;
+let answer = prompt('Сколько вам лет?');
 
-//     return ar.toString().split('').map(Number);
-// }
-// console.log(upArray([0]));
-// // console.log(upArray([4, 3, 2, 5]));
-// // console.log(upArray([1, -9]));
-
+let paste = document.querySelector('.answer');
+paste.textContent = answer;
